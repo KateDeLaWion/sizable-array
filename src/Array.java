787eeed84b8c8e0233bf8597224186dc787c14cc
musicstudio <musicstudio@musics-MBP.home>
2020-04-29 14:23:07 -0400

    update

--- src/Array.java
+++ src/Array.java
@@ -21,6 +21,11 @@ public class Array {
             for (int i= 0; i<count; i++){
                 newItems[i] = items[i];
             }
+            //            No need to expand old array items because once it copies a bigger array,
+//            it will expand itself to accommodate a bigger array.
+//            items = new int[count * 2];
+//            items = newItems;
+//            or
             items = newItems;
         }
 //        items[count] = item;
